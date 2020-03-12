924f1d42e4e9dd958c983b841311cccd94c60c18
Geoffrey Douilly <g3douilly@gmail.com>
2020-03-12 11:53:22 +0100

    test

--- controller/create_task.php
+++ controller/create_task.php
@@ -1,1 +1,28 @@
 <?php
+
+
+use App\Model\App;
+
+require_once("model/App.php");
+
+
+if (!isset($_POST["submit"])) // S'il n'y a pas de formulaire envoyé
+  header("location: /dashboard");
+$auth = App::getAuth();
+$user = $auth->user();
+if (!$user)
+  header("location: /connexion"); // Si l'utilisateur n'est pas connecté
+$id = $user->idUser;
+
+$pdo = App::getPDO();
+
+$title = htmlspecialchars($_POST["title"]);
+$beginDate = isset($_POST["begin-date"]) && strlen($_POST["begin-date"]) > 0 ? htmlspecialchars($_POST["begin-date"]) : date("Y-m-d H:i");
+$endDate = htmlspecialchars($_POST["end-date"]);
+
+$stmt = $pdo->prepare("INSERT INTO Lists(title, dateCreation, dateEnd, idUserOwner) VALUES (:title, :beg, :endDate, :idUser)");
+$stmt->execute(["idUser" => $id, "title" => $title, "beg" => $beginDate, "endDate" => $endDate]);
+$idList = $pdo->lastInsertId();
+$stmt->closeCursor();
+
+header("location: /liste?id=".$idList);
